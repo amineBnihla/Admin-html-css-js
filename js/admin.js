73d9415b5055d9141323d8e569b78f1d9381a9f1
amineBnihla <bnihla.10@gmail.com>
2021-11-11 20:07:42 +0100

    Project html css js admin

---
+++ js/admin.js
@@ -0,0 +1,40 @@
+//resize The side bar
+
+let toggleBtn = document.querySelector('.main .toggle');
+let navigator = document.querySelector('.navigator');
+let mainSection = document.querySelector('.main');
+
+ 
+ 
+   toggleBtn.addEventListener('click',()=>{
+         if(this.innerWidth > 991){ 
+         console.log(this.innerWidth);  
+    navigator.classList.toggle('open');
+    mainSection.classList.toggle('open')
+    
+} else{
+    mainSection.classList.remove('open');
+    navigator.classList.remove('open');
+    navigator.classList.toggle('phone_open');
+}
+   })
+
+
+navigator.addEventListener("click",()=>{
+     if(this.innerWidth <991){    
+         navigator.classList.remove('open')
+         mainSection.classList.remove('open');
+        navigator.classList.toggle('phone_open');
+
+    }
+}) 
+
+//display profile dropdown list
+let profile_btn = document.querySelector("header .profile");
+
+profile_btn.addEventListener("click",()=>{
+    console.log(profile_btn);
+    console.log(profile_btn.lastElementChild)
+
+    profile_btn.lastElementChild.classList.toggle('open_menu');
+});
